1d454c392a31a6a1dbc88109c62d86703247c966
Andi <moepmoep12@gmail.com>
2019-02-18 19:14:56 +0100

    Implemented first logging system based on spdlog

---
+++ code/AIngine/include/public/log.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "spdlog/spdlog.h"
+#include "spdlog/logger.h"
+
+namespace  AIngine {
+
+	class Log {
+
+	public:
+		Log();
+		~Log();
+
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_coreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_clientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_coreLogger;
+		static std::shared_ptr<spdlog::logger> s_clientLogger;
+
+	};
+}
+
+// TODO : Remove from release builds
+
+#define CORE_ERROR(...)  ::AIngine::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define CORE_WARN(...)   ::AIngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define CORE_TRACE(...)  ::AIngine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define CORE_INFO(...)   ::AIngine::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define CORE_FATAL(...)  ::AIngine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+#define DEBUG_ERROR(...)  ::AIngine::Log::GetClientLogger()->error(__VA_ARGS__)
+#define DEBUG_WARN(...)   ::AIngine::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define DEBUG_TRACE(...)  ::AIngine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define DEBUG_INFO(...)   ::AIngine::Log::GetClientLogger()->info(__VA_ARGS__)
+#define DEBUG_FATAL(...)  ::AIngine::Log::GetClientLogger()->fatal(__VA_ARGS__)
---
+++ code/AIngine/src/log.cxx
@@ -0,0 +1,19 @@
+﻿#include "log.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace AIngine {
+
+	std::shared_ptr<spdlog::logger> Log::s_coreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_clientLogger;
+
+	void Log::Init() 
+	{
+		spdlog::set_pattern("%^[%T] %n: %v%$﻿");
+
+		s_coreLogger = spdlog::stdout_color_mt("AIngine");
+		s_coreLogger->set_level(spdlog::level::trace);
+
+		s_clientLogger = spdlog::stdout_color_mt("Application");
+		s_clientLogger->set_level(spdlog::level::trace);
+	}
+}
