cfd5859485d27c72b0ba53c69f044e10f8e2dc88
Andreas Schmidt <a.schmidt@flexus.net>
2019-04-18 17:07:02 +0200

    Added simple openGl config to Window

--- code/AIngine/src/Window.cxx
+++ code/AIngine/src/Window.cxx
@@ -40,6 +40,13 @@ AIngine::Window::Window(const WindowConfig & config)
 
 	SetGLFWCallbacks();
 
+
+	// OpenGL configuration
+	glViewport(0, 0, config.Width, config.Height);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 	CORE_INFO("Window created");
 }
 
