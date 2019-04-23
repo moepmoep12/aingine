97cb0d114e9288158ccb84d9afe66e60aada09d0
Andi <moepmoep12@gmail.com>
2019-04-23 17:30:48 +0200

    Example nodes

--- code/game/src/game.cxx
+++ code/game/src/game.cxx
@@ -45,7 +45,7 @@ void Game::OnAppStartUp()
 {
 	DEBUG_INFO("OnAppStartUp");
 	Application& app = AIngine::Application::Get();
-	std::string path("assets/game/textures/awesomeface.bmp");
+	std::string path("assets/game/textures/awesomeface.png");
 	AIngine::Assets::BitmapAsset* bitmap = app.GetAssetRegistry().Load<AIngine::Assets::BitmapAsset>(path);
 
 	texture = new Texture2D();
@@ -54,16 +54,33 @@ void Game::OnAppStartUp()
 	//m_sceneGraph->AddShape(texture, nullptr);
 	{
 		using namespace AIngine::Rendering;
-		GroupNode* two = m_sceneGraph->AddGroup(nullptr);
-		SceneNode* three = m_sceneGraph->AddShape(texture, two);
-		GroupNode* four = m_sceneGraph->AddGroup(two);
-		GroupNode* five = m_sceneGraph->AddGroup(four);
-		ShapeNode* six = m_sceneGraph->AddShape(nullptr, five);
-		ShapeNode* seven = m_sceneGraph->AddShape(nullptr, five);
-		ShapeNode* eight = m_sceneGraph->AddShape(nullptr, four);
+		//GroupNode* two = m_sceneGraph->AddGroup(nullptr);
+		//SceneNode* three = m_sceneGraph->AddShape(texture, two);
+		//GroupNode* four = m_sceneGraph->AddGroup(two);
+		//GroupNode* five = m_sceneGraph->AddGroup(four);
+		//ShapeNode* six = m_sceneGraph->AddShape(nullptr, five);
+		//ShapeNode* seven = m_sceneGraph->AddShape(nullptr, five);
+		//ShapeNode* eight = m_sceneGraph->AddShape(nullptr, four);
 
-		m_sceneGraph->RemoveNode(five);
-		m_sceneGraph->RemoveNode(eight);
+		//m_sceneGraph->RemoveNode(five);
+		//m_sceneGraph->RemoveNode(eight);
+
+		//GroupNode* two = m_sceneGraph->AddGroup(nullptr);
+		//GroupNode* three = m_sceneGraph->AddGroup(nullptr);
+		//ShapeNode* four = m_sceneGraph->AddShape(texture, two);
+
+
+		ShapeNode* sprite = m_sceneGraph->AddShape(texture);
+		sprite->SetColor(glm::vec3(0.0f, 1.0f, 0.0f));
+		sprite->SetName(std::string("GreenFace"));
+
+		GroupNode* groupTwo = m_sceneGraph->AddGroup(nullptr);
+		groupTwo->SetName(std::string("Parent"));
+
+		ShapeNode* secondSprite = m_sceneGraph->AddShape(texture,groupTwo);
+		secondSprite->SetColor(glm::vec3(1.0f, 0.0f, 0.0f));
+		secondSprite->SetPosition(glm::vec2(500, 300));
+		secondSprite->SetName("RedFace");
 	}
 
 }
