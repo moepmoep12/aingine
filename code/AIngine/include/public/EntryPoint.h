41013021aa6e55e7459627fc3d56fd9ce4b510ec
Andi <moepmoep12@gmail.com>
2019-04-30 18:20:03 +0200

    Added CRT memory leak detection

--- code/AIngine/include/public/EntryPoint.h
+++ code/AIngine/include/public/EntryPoint.h
@@ -2,9 +2,13 @@
 
 // the Application created wih AIngine implements the function
 extern AIngine::Application* AIngine::CreateApplication();
-
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
 int main(int argc, char** argv) 
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	AIngine::Log::Init();
 	CORE_INFO("Initalized Logging");
 
@@ -17,6 +21,5 @@ int main(int argc, char** argv)
 
 	spdlog::drop_all();
 	spdlog::shutdown();
-
 	return 0;
 }
