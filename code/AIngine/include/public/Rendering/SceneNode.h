628f5ad89354d40f817084a932b734bf15418a69
Andi <moepmoep12@gmail.com>
2019-05-05 16:44:06 +0200

    SceneNodes are no longer used

--- code/AIngine/include/public/Rendering/SceneNode.h
+++ code/AIngine/include/public/Rendering/SceneNode.h
@@ -1,66 +1,41 @@
 #pragma once
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <vector>
-#include "texture.h"
+//#include <glm/glm.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+//#include <vector>
+//#include "texture.h"
+//#include "GameObject.h"
 
 namespace AIngine::Rendering {
 
-	class Traverser;
+	//class Traverser;
 
-	class SceneNode {
-	public:
+	//class SceneNode {
+	//public:
 
-		SceneNode() = delete;
-		SceneNode(SceneNode* parent, const std::string& name)
-			: m_parent(parent), m_name(name) {}
-		SceneNode(SceneNode* parent, const std::string& name, const glm::vec2& position)
-			: m_parent(parent), m_name(name) {
-			SetPosition(position);
-		}
-		SceneNode(SceneNode* parent, const std::string& name, const glm::vec2& position, const glm::vec2& scale)
-			: m_parent(parent), m_name(name) {
-			SetPosition(position); SetScale(scale);
-		}
-		SceneNode(SceneNode* parent, const std::string& name, const glm::vec2& position, const glm::vec2& scale, const float rotation)
-			: m_parent(parent), m_name(name) {
-			SetPosition(position); SetScale(scale); SetRotation(rotation);
-		}
+	//	SceneNode() = delete;
+	//	SceneNode(SceneNode* parent, GameObject* obj)
+	//		: m_parent(parent), m_gameObject(obj) {}
 
-		virtual bool Accept(Traverser& traverser) = 0;
 
-		inline virtual const std::string& GetName() const { return m_name; }
-		inline void SetName(const std::string& name) { m_name = name; }
+	//	inline SceneNode& GetParent() { return *m_parent; }
+	//	inline void SetParent(SceneNode* parent) { m_parent = parent; }
 
-		inline SceneNode& GetParent() { return *m_parent; }
-		inline void SetParent(SceneNode* parent) { m_parent = parent; }
+	//	inline const std::vector<SceneNode*>& GetChildren() { return m_children; }
 
-		inline glm::mat4& GetTransform() { return m_transform; }
-		inline void SetTransform(const glm::mat4& transform) { m_transform = transform; }
+	//	virtual bool Accept(Traverser& traverser);
+	//	virtual void AddChild(SceneNode* node);
+	//	virtual void RemoveChild(SceneNode* node);
 
-		inline glm::vec2& GetPosition() { return m_position; }
-		inline void SetPosition(const glm::vec2 position) { m_position = position; /*glm::translate(m_transform, glm::vec3(m_position, 0.0f));*/ }
+	//protected:
+	//	GameObject* m_gameObject;
+	//	SceneNode* m_parent;
+	//	std::vector<SceneNode*> m_children;
+	//private:
 
-		inline glm::vec2& GetScale() { return m_scale; }
-		inline void SetScale(const glm::vec2 scale) { m_scale = scale;/* glm::scale(m_transform, glm::vec3(m_scale, 1.0f)); */ }
+	//};
 
-		inline float& GetRotation() { return m_rotation; }
-		inline void SetRotation(float rot) { m_rotation = rot;/*  glm::rotate(m_transform, m_rotation, glm::vec3(0.0f, 0.0f, 1.0f));*/ }
-
-	protected:
-		std::string m_name;
-		glm::mat4 m_transform = glm::mat4(1.0f);
-		glm::vec2 m_position = glm::vec2(0.0f);
-		glm::vec2 m_scale = glm::vec2(1.0f);
-		float m_rotation = 0.0f;
-		SceneNode* m_parent;
-
-	private:
-
-	};
-
-	class GroupNode : public SceneNode {
+	/*class GroupNode : public SceneNode {
 	public:
 
 		using SceneNode::SceneNode;
@@ -100,5 +75,5 @@ namespace AIngine::Rendering {
 		Texture2D* m_texture;
 		glm::vec2 m_size;
 		glm::vec3 m_spriteColor;
-	};
+	};*/
 }
--- code/AIngine/src/Rendering/SceneNodes.cxx
+++ code/AIngine/src/Rendering/SceneNodes.cxx
@@ -1,92 +1,143 @@
-#include "Rendering/SceneNode.h"
-#include "Rendering/Traverser.h"
-#include "log.h"
+//#include "Rendering/SceneNode.h"
+//#include "Traverser.h"
+//#include "log.h"
 
 namespace AIngine::Rendering {
 
-	ShapeNode::ShapeNode(Texture2D* texture, GroupNode* parent, const std::string& name, const glm::vec2& position, const glm::vec2& scale, const float rotation, const glm::vec3& color)
-		:  SceneNode(parent,name), m_texture(texture), m_spriteColor(color)
-	{
-		SetPosition(position);
-		SetScale(scale);
-		SetRotation(rotation);	
-	}
+	//ShapeNode::ShapeNode(Texture2D* texture, GroupNode* parent, const std::string& name, const glm::vec2& position, const glm::vec2& scale, const float rotation, const glm::vec3& color)
+	//	:  SceneNode(parent,name), m_texture(texture), m_spriteColor(color)
+	//{
+	//	SetPosition(position);
+	//	SetScale(scale);
+	//	SetRotation(rotation);	
+	//}
 
-	ShapeNode::~ShapeNode()
-	{
-		CORE_INFO("Destroying {0}", GetName());
-	}
+	//ShapeNode::~ShapeNode()
+	//{
+	//	CORE_INFO("Destroying {0}", GetName());
+	//}
 
-	bool ShapeNode::Accept(Traverser& traverser) {
-		return traverser.Visit(*this);
-	}
+	//bool ShapeNode::Accept(Traverser& traverser) {
+	//	return traverser.Visit(*this);
+	//}
 
-	glm::vec2 & ShapeNode::GetSize()
-	{
-		m_size = glm::vec2(m_texture->Width * m_scale.x, m_texture->Height * m_scale.y);
-		return m_size;
-	}
+	//glm::vec2 & ShapeNode::GetSize()
+	//{
+	//	m_size = glm::vec2(m_texture->Width * m_scale.x, m_texture->Height * m_scale.y);
+	//	return m_size;
+	//}
 
-	/************************************ GROUP NODE ******************************/
+	///************************************ GROUP NODE ******************************/
 
-	GroupNode::~GroupNode()
-	{
-		//for (unsigned int i = 0; i < m_children.size(); ++i) {
-		//	delete m_children[i];
-		//}
-		CORE_INFO("Destroying {0}", GetName());
-		m_children.clear();
-	}
+	//GroupNode::~GroupNode()
+	//{
+	//	//for (unsigned int i = 0; i < m_children.size(); ++i) {
+	//	//	delete m_children[i];
+	//	//}
+	//	CORE_INFO("Destroying {0}", GetName());
+	//	m_children.clear();
+	//}
 
-	bool GroupNode::Accept(Traverser& traverser) {
-		if (traverser.Enter(*this)) {
-			for (auto i = m_children.begin(); i != m_children.end(); ++i)
-			{
-				SceneNode* node = *i._Ptr;
-				if (!node->Accept(traverser)) {
-					break;
-				}
-			}
-		}
+	//bool GroupNode::Accept(Traverser& traverser) {
+	//	if (traverser.Enter(*this)) {
+	//		for (auto i = m_children.begin(); i != m_children.end(); ++i)
+	//		{
+	//			SceneNode* node = *i._Ptr;
+	//			if (!node->Accept(traverser)) {
+	//				break;
+	//			}
+	//		}
+	//	}
 
-		return traverser.Leave(*this);
-	}
-	void GroupNode::AddChild(SceneNode * node)
-	{
-		if (node) {
-			m_children.push_back(node);
-			node->SetParent(this);
-		}
-	}
-	void GroupNode::RemoveChild(SceneNode * node)
-	{
-		auto it = m_children.begin();
-		while (it != m_children.end())
-		{
-			if (*it == node)
-			{
-				it = m_children.erase(it);
-				node->SetParent(nullptr);
-			}
-			else
-				it++;
-		}
-	}
-	unsigned int GroupNode::ChildrenInSubTree()
-	{
-		int count = 0;
+	//	return traverser.Leave(*this);
+	//}
+	//void GroupNode::AddChild(SceneNode * node)
+	//{
+	//	if (node) {
+	//		m_children.push_back(node);
+	//		node->SetParent(this);
+	//	}
+	//}
+	//void GroupNode::RemoveChild(SceneNode * node)
+	//{
+	//	auto it = m_children.begin();
+	//	while (it != m_children.end())
+	//	{
+	//		if (*it == node)
+	//		{
+	//			it = m_children.erase(it);
+	//			node->SetParent(nullptr);
+	//		}
+	//		else
+	//			it++;
+	//	}
+	//}
+	//unsigned int GroupNode::ChildrenInSubTree()
+	//{
+	//	int count = 0;
 
-		for (auto i = m_children.begin(); i != m_children.end(); ++i)
-		{
-			GroupNode* node = dynamic_cast<GroupNode*>(*i._Ptr);
-			if (node) {
-				count += node->ChildrenInSubTree();
-			}
+	//	for (auto i = m_children.begin(); i != m_children.end(); ++i)
+	//	{
+	//		GroupNode* node = dynamic_cast<GroupNode*>(*i._Ptr);
+	//		if (node) {
+	//			count += node->ChildrenInSubTree();
+	//		}
 
-		}
+	//	}
 
-		count += m_children.size();
+	//	count += m_children.size();
 
-		return count;
-	}
+	//	return count;
+	//}
+
+	//bool SceneNode::Accept(Traverser& traverser)
+	//{
+	//	if (m_children.size() == 0)
+	//	{
+	//		return traverser.Visit(*this);
+	//	}
+	//	else
+	//	{
+	//		if (traverser.Enter(*this))
+	//		{
+	//			for (auto i = m_children.begin(); i != m_children.end(); ++i)
+	//			{
+	//				SceneNode* node = *i._Ptr;
+	//				if (!node->Accept(traverser)) {
+	//					break;
+	//				}
+	//			}
+	//		}
+	//		return traverser.Leave(*this);
+	//	}
+	//}
+	//void SceneNode::AddChild(SceneNode * node)
+	//{
+	//	if (node) {
+	//		m_children.push_back(node);
+	//		node->SetParent(this);
+	//	}
+	//}
+	//void SceneNode::RemoveChild(SceneNode * node)
+	//{
+	//	auto it = m_children.begin();
+	//	while (it != m_children.end())
+	//	{
+	//		if (*it == node)
+	//		{
+	//			it = m_children.erase(it);
+	//			node->SetParent(nullptr);
+	//		}
+	//		else
+	//			it++;
+	//	}
+	//}
+	//SceneNode::~SceneNode()
+	//{
+	//	for (unsigned int i = 0; i < m_children.size(); ++i) {
+	//		delete m_children[i];
+	//	}
+	//	CORE_INFO("Destroying {0}", m_gameObject->GetName());
+	//	m_children.clear();
+	//}
 }
