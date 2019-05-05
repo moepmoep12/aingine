5511238fe72c65399ca6e9fc23b398e71105e56e
Andi <moepmoep12@gmail.com>
2019-05-05 16:44:40 +0200

    Updated Texture2D

--- code/AIngine/include/public/Rendering/texture.h
+++ code/AIngine/include/public/Rendering/texture.h
@@ -1,54 +1,47 @@
 #pragma once
 
 #include <glad/glad.h>
-
+#include <glm/glm.hpp>
 #include "shader.h"
 #include "bitmap.h"
+#include "Component.h"
 
-class Texture {
-public:
-	Texture();
+namespace AIngine::Rendering {
 
-	~Texture();
+	class Texture2D : public AIngine::Component {
+	public:
+		// Holds the ID of the texture object, used for all texture operations to reference to this particlar texture
+		GLuint ID;
 
-	void SetTexture(Bitmap &texture);
+		// Texture image dimensions
+		GLuint Width, Height; // Width and height of loaded image in pixels
 
-	void Bind(GLShaderProgram *sp);
+		// Texture Format
+		GLuint Internal_Format; // Format of texture object
+		GLuint Image_Format; // Format of loaded image
 
-	void Unbind(GLShaderProgram *sp);
+		// Texture configuration
+		GLuint Wrap_S; // Wrapping mode on S axis
+		GLuint Wrap_T; // Wrapping mode on T axis
+		GLuint Filter_Min; // Filtering mode if texture pixels < screen pixels
+		GLuint Filter_Max; // Filtering mode if texture pixels > screen pixels
 
-private:
-	GLuint textureBuffer = GL_NONE;
-	GLuint trilinearSampler = GL_NONE;
-};
+		// Constructor (sets default texture modes)
+		Texture2D(GameObject* owner);
+		virtual ~Texture2D() ;
 
-// from learnopengl.com
-class Texture2D {
-public:
-	// Holds the ID of the texture object, used for all texture operations to reference to this particlar texture
-	GLuint ID;
+		// Generates texture from a bitmap
+		void Generate(Bitmap &texture);
 
-	// Texture image dimensions
-	GLuint Width, Height; // Width and height of loaded image in pixels
+		// Binds the texture as the current active GL_TEXTURE_2D texture object
+		void Bind() const;
 
-	// Texture Format
-	GLuint Internal_Format; // Format of texture object
-	GLuint Image_Format; // Format of loaded image
+		inline const glm::vec3& GetColor() const { return m_spriteColor; }
+		inline void SetColor(const glm::vec3& color) { m_spriteColor = color; }
 
-	// Texture configuration
-	GLuint Wrap_S; // Wrapping mode on S axis
-	GLuint Wrap_T; // Wrapping mode on T axis
-	GLuint Filter_Min; // Filtering mode if texture pixels < screen pixels
-	GLuint Filter_Max; // Filtering mode if texture pixels > screen pixels
+	private:
+		glm::vec3 m_spriteColor;
 
-	// Constructor (sets default texture modes)
-	Texture2D();
-	~Texture2D();
-
-	// Generates texture from a bitmap
-	void Generate(Bitmap &texture);
-
-	// Binds the texture as the current active GL_TEXTURE_2D texture object
-	void Bind() const;
-};
+	};
+}
 
--- code/AIngine/src/Rendering/texture.cxx
+++ code/AIngine/src/Rendering/texture.cxx
@@ -1,95 +1,46 @@
 #include "Rendering/texture.h"
 
-Texture::Texture() {
-    glGenTextures(1, &textureBuffer);
-    glGenSamplers(1, &trilinearSampler);
+namespace AIngine::Rendering {
+	Texture2D::Texture2D(GameObject* owner)
+		: Width(0), Height(0), Internal_Format(GL_RGB), Image_Format(GL_RGB), Wrap_S(GL_REPEAT), Wrap_T(GL_REPEAT), Filter_Min(GL_LINEAR), Filter_Max(GL_LINEAR), m_spriteColor(glm::vec3(1.0)), Component(owner)
+	{
+		glGenTextures(1, &this->ID);
+	}
 
-    glSamplerParameteri(trilinearSampler, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glSamplerParameteri(trilinearSampler, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glSamplerParameteri(trilinearSampler, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glSamplerParameteri(trilinearSampler, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-}
+	Texture2D::~Texture2D() 
+	{
+		glDeleteTextures(1, &this->ID);
+	}
 
-Texture::~Texture() {
-    glDeleteTextures(1, &textureBuffer);
-    glDeleteSamplers(1, &trilinearSampler);
-}
+	void Texture2D::Generate(Bitmap &texture)
+	{
+		this->Width = texture.GetWidth();
+		this->Height = texture.GetHeight();
+		this->Image_Format = (texture.GetChannels() == 3) ? GL_RGB : GL_RGBA;
+		this->Internal_Format = (texture.GetChannels() == 3) ? GL_RGB : GL_RGBA;
+		// Create Texture
+		glBindTexture(GL_TEXTURE_2D, this->ID);
+		glTexImage2D(GL_TEXTURE_2D,
+			0,
+			this->Internal_Format,
+			Width,
+			Height,
+			0,
+			this->Image_Format,
+			GL_UNSIGNED_BYTE,
+			texture.GetData());
 
-void Texture::SetTexture(Bitmap &texture) {
-    glBindTexture(GL_TEXTURE_2D, textureBuffer);
-    glTexImage2D(GL_TEXTURE_2D,
-                 0,
-                 GL_SRGB8,
-                 texture.GetWidth(),
-                 texture.GetHeight(),
-                 0,
-                 (texture.GetChannels() == 3) ? GL_RGB : GL_RGBA,
-                 GL_UNSIGNED_BYTE,
-                 texture.GetData());
-    glGenerateMipmap(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, 0);
-}
+		// Set Texture wrap and filter modes
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, this->Wrap_S);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, this->Wrap_T);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, this->Filter_Min);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, this->Filter_Max);
+		// Unbind texture
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
 
-void Texture::Bind(GLShaderProgram * sp) {
-    const GLint colorTextureUniform = glGetUniformLocation(sp->GetID(), "colorTexture");
-
-    if (colorTextureUniform != -1) {
-        glActiveTexture(GL_TEXTURE0);
-        glBindSampler(0, trilinearSampler);
-        glBindTexture(GL_TEXTURE_2D, textureBuffer);
-    }
-}
-
-void Texture::Unbind(GLShaderProgram *sp) {
-    const GLint colorTextureUniform = glGetUniformLocation(sp->GetID(), "colorTexture");
-
-    if (colorTextureUniform != -1) glDisableVertexAttribArray(colorTextureUniform);
-    glBindTexture(GL_TEXTURE_2D, 0);
-    glBindSampler(0, 0);
-}
-
-
-/********************************************************************************************/
-
-Texture2D::Texture2D()
-	: Width(0), Height(0), Internal_Format(GL_RGB), Image_Format(GL_RGB), Wrap_S(GL_REPEAT), Wrap_T(GL_REPEAT), Filter_Min(GL_LINEAR), Filter_Max(GL_LINEAR)
-{
-	glGenTextures(1, &this->ID);
-}
-
-Texture2D::~Texture2D()
-{
-	glDeleteTextures(1, &this->ID);
-}
-
-void Texture2D::Generate(Bitmap &texture)
-{
-	this->Width = texture.GetWidth();
-	this->Height = texture.GetHeight();
-	this->Image_Format = (texture.GetChannels() == 3) ? GL_RGB : GL_RGBA;
-	this->Internal_Format = (texture.GetChannels() == 3) ? GL_RGB : GL_RGBA;
-	// Create Texture
-	glBindTexture(GL_TEXTURE_2D, this->ID);
-	glTexImage2D(GL_TEXTURE_2D,
-		0,
-		this->Internal_Format,
-		Width,
-		Height,
-		0,
-		this->Image_Format,
-		GL_UNSIGNED_BYTE,
-		texture.GetData());
-
-	// Set Texture wrap and filter modes
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, this->Wrap_S);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, this->Wrap_T);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, this->Filter_Min);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, this->Filter_Max);
-	// Unbind texture
-	glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-void Texture2D::Bind() const
-{
-	glBindTexture(GL_TEXTURE_2D, this->ID);
+	void Texture2D::Bind() const
+	{
+		glBindTexture(GL_TEXTURE_2D, this->ID);
+	}
 }
