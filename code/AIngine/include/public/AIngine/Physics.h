7c6718773470035ba581898f21cd14c1d96e1843
Andi <moepmoep12@gmail.com>
2019-05-20 22:28:16 +0200

    Added methods for applying forces to bodies

--- code/AIngine/include/public/AIngine/Physics.h
+++ code/AIngine/include/public/AIngine/Physics.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "AIngine/Component.h"
 #include <Box2D/Box2D.h>
+#include <glm/glm.hpp>
 
 namespace AIngine::Editor {
 	class SceneGraphWidget;
@@ -19,7 +20,16 @@ namespace AIngine {
 		// inherited via Component
 		virtual void OnUpdate(float deltatime) override;
 
-		void CreateBody( b2BodyDef& bodydef, b2FixtureDef& fixtureDef);
+		void CreateBody(b2BodyDef& bodydef, b2FixtureDef& fixtureDef);
+
+		void ApplyForce(const glm::vec2& force, const glm::vec2& point);
+		void ApplyForce(const b2Vec2& force, const b2Vec2& point);
+		void ApplyForceToCenter(const glm::vec2& force);
+		void ApplyForceToCenter(const b2Vec2& force);
+		void ApplyLinearImpulseToCenter(const glm::vec2& impulse);
+		void ApplyLinearImpulseToCenter(const b2Vec2& impulse);
+
+		void UpdateTransform();
 
 	private:
 		b2Body* m_body;
--- code/AIngine/src/AIngine/Physics.cxx
+++ code/AIngine/src/AIngine/Physics.cxx
@@ -22,7 +22,7 @@ namespace AIngine {
 
 	PhysicsComponent::~PhysicsComponent()
 	{
-		if (m_body) {
+		if (m_body && AIngine::World::s_instance) {
 			AIngine::World::s_instance->m_physicsWorld->DestroyBody(m_body);
 		}
 	}
@@ -43,5 +43,38 @@ namespace AIngine {
 		bodydef.position.Set(worldPos.x, worldPos.y);
 		m_body = AIngine::World::CreateBody(bodydef);
 		m_body->CreateFixture(&fixtureDef);
+	}
+
+
+	void PhysicsComponent::ApplyForce(const glm::vec2 & force, const glm::vec2 & point)
+	{
+		ApplyForce(b2Vec2(force.x, force.y), b2Vec2(point.x, point.y));
+	}
+	void PhysicsComponent::ApplyForce(const b2Vec2 & force, const b2Vec2 & point)
+	{
+		m_body->ApplyForce(force, point, true);
+	}
+	void PhysicsComponent::ApplyForceToCenter(const glm::vec2 & force)
+	{
+		ApplyForceToCenter(b2Vec2(force.x, force.y));
+	}
+	void PhysicsComponent::ApplyForceToCenter(const b2Vec2 & force)
+	{
+		m_body->ApplyForceToCenter(force, true);
+	}
+	void PhysicsComponent::ApplyLinearImpulseToCenter(const glm::vec2 & impulse)
+	{
+		ApplyLinearImpulseToCenter(b2Vec2(impulse.x, impulse.y));
+	}
+	void PhysicsComponent::ApplyLinearImpulseToCenter(const b2Vec2 & impulse)
+	{
+		m_body->ApplyLinearImpulseToCenter(impulse, true);
+	}
+	void PhysicsComponent::UpdateTransform()
+	{
+		if (m_body) {
+			glm::vec2 worldPos = m_owner->GetWorldPosition();
+			m_body->SetTransform(b2Vec2(worldPos.x, worldPos.y), m_owner->GetWorldRotation());
+		}
 	}
 }
