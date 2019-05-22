3f8bf85c4ec986f52aa3c96424bbf422e4bba597
Andi <moepmoep12@gmail.com>
2019-05-22 23:11:23 +0200

    Created Rectangle class

---
+++ code/AIngine/include/public/Structures/Rectangle.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace AIngine::Structures {
+	struct Rectangle {
+
+		Rectangle()
+			: x(0), y(0), width(0), height(0) {}
+
+		Rectangle(float x, float y, float width, float height)
+			: x(x), y(y), width(width), height(height) {}
+
+		bool Contains(const glm::vec2& point) const {
+			return (point.x > x && point.x < x + width)
+				&& (point.y > y && point.y < y + height);
+		}
+
+		// corresponds to the top left corner
+		glm::vec2 GetPosition() const {
+			return glm::vec2(x, y);
+		}
+
+		void SetPosition(const glm::vec2& pos) {
+			x = pos.x;
+			y = pos.y;
+		}
+
+		glm::vec2 GetTopRight() const {
+			return glm::vec2(x + width, y);
+		}
+
+		glm::vec2 GetBottomLeft() const {
+			return glm::vec2(x, y + height);
+		}
+
+		glm::vec2 GetMax() const {
+			return glm::vec2(x + width, y + height);
+		}
+
+		float Area() const {
+			return width * height;
+		}
+
+		enum Corner {
+			TopLeft = 0,
+			TopRight = 1,
+			BottomRight = 2,
+			BottomLeft = 3
+		};
+
+		float x, y, width, height;
+	};
+}
