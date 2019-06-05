28bc7a294c29e0245b954402dd6e82cca3be6018
Andreas Schmidt <a.schmidt@flexus.net>
2019-06-05 11:10:48 +0200

    Removed unused files

--- code/AIngine/include/public/Rendering/meshloader.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "mesh.h"
-#include <string>
-
-void LoadOBJ(std::string const &assetPath, AIngine::Renderer::Mesh * mesh);
--- code/AIngine/include/public/Rendering/mvp.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <glm/vec2.hpp>
-#include <glm/mat4x4.hpp>
-
-glm::mat4 CalculateMVP(float Translate, glm::vec2 const &Rotate);
