015884b0d621c9dda7cbe33afb9a057afcdbccc8
Andi <moepmoep12@gmail.com>
2019-06-10 16:48:04 +0200

    Created new CrappyBird project and renamed the old one to CrappyBird_old

--- code/CrappyBird/include/CrappyBird.h
+++ code/CrappyBird/include/CrappyBird.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "AIngine/Core.h"
 #include "Rendering/texture.h"
-#include "Player.h"
 
 
 namespace CrappyBird {
@@ -10,35 +9,12 @@ namespace CrappyBird {
 	public:
 
 		// Inherited via Application
-		virtual void OnAppStartUp() override;
-		virtual void OnAppShutDown() override;
-		virtual void OnAppUpdate() override;
-		virtual void OnAppEvent(AIngine::Events::EventData& e) override;
+		virtual void OnAppStartUp() override {}
+		virtual void OnAppShutDown() override {}
+		virtual void OnAppUpdate() override {}
+		virtual void OnAppEvent(AIngine::Events::EventData& e) override {}
 
 		CrappyBird();
 		virtual ~CrappyBird();
-
-	private:
-		void CreateGround();
-		void CreatePlayer();
-		void SpawnObstacles();
-		void CreateBackGround();
-		void UpdateBackGround();
-		void MoveObstacles();
-		void RestartGame();
-
-		bool isGameOver();
-
-	private:
-		AIngine::Rendering::Texture2D* m_backGroundSky;
-		AIngine::Rendering::Texture2D* m_backGroundClouds;
-		AIngine::Rendering::Texture2D* m_backGroundClouds2;
-		AIngine::Rendering::Texture2D* m_backGroundHills[4];
-		glm::vec2 m_playerSize;
-		AIngine::GameObject* m_obstacleParent;
-		AIngine::GameObject* m_player;
-		float32 m_bottomGroundPosY;
-		float32 m_ceilingPosY;
-		bool m_running = true;
 	};
 }
