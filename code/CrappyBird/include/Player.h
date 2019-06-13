d855deaff5586cb7deae0f7cea8b337caec2ef5a
Andreas Schmidt <a.schmidt@flexus.net>
2019-06-13 17:52:33 +0200

    Created first Script in Crappybird: Player

--- code/CrappyBird/include/Player.h
+++ code/CrappyBird/include/Player.h
@@ -1,12 +1,19 @@
 #pragma once
+#include "AIngine/Script.h"
 #include "AIngine/GameObject.h"
+#include "AIngine/Physics.h"
 
 namespace CrappyBird {
-	class Player : public AIngine::GameObject
+	class Player : public AIngine::Script
 	{
 	public:
-		Player(AIngine::GameObject* parent, const std::string& name);
-		Player() = delete;
+		Player(AIngine::GameObject* owner);
+		virtual void OnStart() override;
+		virtual void OnEvent(AIngine::Events::EventData& e) override;
+		virtual void Update(float deltatime) override;
 		virtual ~Player();
+
+	private:
+		AIngine::Physics::PhysicsComponent* m_physBody;
 	};
 }
--- code/CrappyBird/src/Player.cxx
+++ code/CrappyBird/src/Player.cxx
@@ -1,15 +1,35 @@
 #include "Player.h"
-#include "Rendering/texture.h"
-#include <string>
-#include "AIngine/Physics.h"
-#include "Application.h"
+#include "AIngine/Input.h"
+#include "Events/Event.h"
+#include "Events/InputEvents.h"
+
 
 namespace CrappyBird {
-
-	CrappyBird::Player::Player(AIngine::GameObject * parent, const std::string & name)
-		: AIngine::GameObject(parent, name)
+	Player::Player(AIngine::GameObject * owner)
+		: Script(owner)
 	{
+	}
 
+	void Player::OnStart()
+	{
+		m_physBody = m_owner->GetComponent<AIngine::Physics::PhysicsComponent>();
+	}
+
+	void Player::OnEvent(AIngine::Events::EventData & e)
+	{
+		// key pressed
+		if (typeid(e) == typeid(AIngine::Events::KeyPressedEvent::KeyPressedEventData)) {
+			AIngine::Events::KeyPressedEvent::KeyPressedEventData pressedEvent = dynamic_cast<AIngine::Events::KeyPressedEvent::KeyPressedEventData&>(e);
+			if (pressedEvent.GetKeyCode() == AIngine::KeyCodes::R) {
+			}
+		}
+	}
+
+	void Player::Update(float deltatime)
+	{
+		if (AIngine::Input::IsMouseButtonPressed(0)) {
+			m_physBody->ApplyLinearImpulseToCenter(glm::vec2(0, -0.075f));
+		}
 	}
 
 	CrappyBird::Player::~Player()
