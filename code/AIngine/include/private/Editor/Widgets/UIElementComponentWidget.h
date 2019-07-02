99656271461df1e38aa309d6c47cce5ab799a106
Andi <moepmoep12@gmail.com>
2019-07-02 22:19:40 +0200

    Added anchor combo

--- code/AIngine/include/private/Editor/Widgets/UIElementComponentWidget.h
+++ code/AIngine/include/private/Editor/Widgets/UIElementComponentWidget.h
@@ -3,7 +3,10 @@
 
 namespace AIngine::Editor::Widget::Component {
 
+	void AnchorCombo(AIngine::UI::UIElement* element);
+
 	void ChangeTransform(AIngine::UI::UIElement* element);
+
 	void ColorEdit(float* Color, const char* title);
 
 }
--- code/AIngine/src/Editor/Widgets/UIElementComponentWidget.cxx
+++ code/AIngine/src/Editor/Widgets/UIElementComponentWidget.cxx
@@ -1,14 +1,88 @@
 #include "Editor/Widgets/UIElementComponentWidget.h"
 #include "UI/UIELement.h"
+#include "AIngine/Graphics.h"
+#include "Rendering/Viewport.h"
+#include "Application.h"
 
 #include "imgui.h"
 
 namespace AIngine::Editor::Widget::Component {
 
+	void AnchorCombo(AIngine::UI::UIElement * element)
+	{
+		static const AIngine::UI::Anchor choosableAnchors[] = { AIngine::UI::Anchor::Center,AIngine::UI::Anchor::TopLeft, AIngine::UI::Anchor::TopRight, AIngine::UI::Anchor::BottomRight,
+			AIngine::UI::Anchor::BottomLeft, AIngine::UI::Anchor::CenterUp,AIngine::UI::Anchor::CenterLeft, AIngine::UI::Anchor::CenterDown, AIngine::UI::Anchor::CenterRight };
+		static const char* anchorNames[] = { "Center", "TopLeft", "TopRight", "BottomRight", "BottomLeft", "CenterUp", "CenterLeft", "CenterDown", "CenterRight" };
+
+		/*AIngine::UI::Anchor currentAnchor = element->AnchorPos;*/
+		if (ImGui::BeginCombo("Anchor", anchorNames[element->GetAnchor()])) {
+			for (int i = 0; i < IM_ARRAYSIZE(anchorNames); i++) {
+				bool isSelected = choosableAnchors[i] == element->GetAnchor();
+				if (ImGui::Selectable(anchorNames[i], isSelected))
+					element->SetAnchor( choosableAnchors[i]);
+				if (isSelected)
+					ImGui::SetItemDefaultFocus();
+			}
+			ImGui::EndCombo();
+		}
+
+		glm::vec2 anchorpos;
+		const AIngine::Structures::RectangleI& viewportRect = AIngine::Application::GetViewport().GetRectangle();
+
+
+		switch (element->GetAnchor()) {
+		case AIngine::UI::Anchor::TopLeft:
+			anchorpos = glm::vec2(0);
+			break;
+
+		case AIngine::UI::Anchor::TopRight:
+			anchorpos = viewportRect.GetTopRight();
+			break;
+
+		case AIngine::UI::Anchor::BottomRight:
+			anchorpos = viewportRect.GetMax();
+			break;
+
+		case AIngine::UI::Anchor::BottomLeft:
+			anchorpos = viewportRect.GetBottomLeft();
+			break;
+
+		case AIngine::UI::Anchor::Center:
+			anchorpos = viewportRect.GetCenter();
+			break;
+
+		case AIngine::UI::Anchor::CenterDown:
+			glm::highp_ivec2 centerDown = viewportRect.GetMax();
+			centerDown.x -= viewportRect.width * 0.5f;
+			anchorpos = centerDown;
+			break;
+
+		case AIngine::UI::Anchor::CenterLeft:
+			glm::highp_ivec2 centerLeft = viewportRect.GetPosition();
+			centerLeft.y += viewportRect.height * 0.5f;
+			anchorpos = centerLeft;
+			break;
+
+		case AIngine::UI::Anchor::CenterRight:
+			glm::highp_ivec2 centerRight = viewportRect.GetTopRight();
+			centerRight.y += viewportRect.height * 0.5f;
+			anchorpos = centerRight;
+			break;
+
+		case AIngine::UI::Anchor::CenterUp:
+			glm::highp_ivec2 centerUp = viewportRect.GetTopRight();
+			centerUp.x -= viewportRect.width * 0.5f;
+			anchorpos = centerUp;
+			break;
+		}
+
+		AIngine::Graphics::PointScreen(anchorpos, 50 , glm::vec3(0.35, 0.72, 0));
+	}
+
 	void ChangeTransform(AIngine::UI::UIElement * element)
 	{
-		int pos[] = { element->GetRectangle().x, element->GetRectangle().y };
-		int dim[] = { element->GetRectangle().width, element->GetRectangle().height };
+		int pos[] = { element->GetRectangleNative().x, element->GetRectangleNative().y };
+		int dim[] = { element->GetRectangleNative().width, element->GetRectangleNative().height };
 		if (ImGui::DragInt2("Screen Position", pos)) {
 			element->SetPosition(glm::vec2(pos[0], pos[1]));
 		}
