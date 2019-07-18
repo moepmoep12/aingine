9936c8db721e581520da7143c51e452c29ae2df5
Andreas Schmidt <a.schmidt@flexus.net>
2019-07-18 16:55:19 +0200

    Forklifts: Implemented specific A* for particular use case

--- code/Projects/Forklifts/code/include/Graph.h
+++ code/Projects/Forklifts/code/include/Graph.h
@@ -2,6 +2,11 @@
 #include "AIngine/Core.h"
 #include "Edge.h"
 #include "Node.h"
+#include "Tasks.h"
+
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
 
 namespace Forklifts {
 
@@ -98,4 +103,94 @@ namespace Forklifts {
 		glm::vec2 scale;
 
 	};
+
+	template<typename N, typename E>
+	bool Search(IGraphNode<N, E>* startNode, Task& task)
+	{
+		struct SearchValues {
+			float gScore;
+			float fScore;
+		};
+
+		task.Cost = std::numeric_limits<float>::max();
+
+		// keep track of the nodes we haven't visited yet
+		std::vector<IGraphNode<N, E>*> openSet;
+
+		// keep track of the visited nodes
+		std::unordered_set<IGraphNode<N, E>*> closedSet;
+
+		// Map of nodes to keep track of the path taken
+		std::unordered_map<IGraphNode<N, E>*, IGraphNode<N, E>*> cameFrom;
+
+		// map of nodes and their search values used in A*
+		std::unordered_map<IGraphNode<N, E>*, SearchValues> searchValueSet;
+
+		openSet.push_back(startNode);
+		searchValueSet[startNode] = SearchValues{ 0, task.Heuristic(startNode) };
+
+		while (openSet.size() > 0) {
+
+			// sort the openSet by the nodes' fScores
+			std::sort(openSet.begin(), openSet.end(), [&searchValueSet](IGraphNode<N, E>* v, IGraphNode<N, E> * u) -> bool
+			{ return searchValueSet[v].fScore < searchValueSet[u].fScore; });
+
+			// current node is the node with the lowest fScore
+			IGraphNode<NodeData, EdgeData>* current = openSet[0];
+
+			// check if goal was reached yet and construct path if true
+			if (task.GoalTest(current))
+			{
+				ReconstructPath(cameFrom, current, task.Path, &task.Cost);
+				return true;
+			}
+
+			// node wasn't goal, remove if from the open set and add it to the closed set
+			openSet.erase(openSet.begin());
+			closedSet.emplace(current);
+
+			for (auto& neighbor : current->m_neighbors) {
+
+				if (closedSet.find(neighbor.first) != closedSet.end()) {
+					continue; // ignore neighbour which is already evaluated
+				}
+
+				if (searchValueSet.find(neighbor.first) == searchValueSet.end())
+				{
+					openSet.push_back(neighbor.first); // discover a new node
+					searchValueSet[neighbor.first] = SearchValues{ std::numeric_limits<float>::max(), std::numeric_limits<float>::max() };
+				}
+
+				// the new gScore is the distance traveled so far + the distance to the neighbor
+				float gScore_temp = searchValueSet[current].gScore + current->GetEdge(neighbor.first)->Cost;
+
+				// no pathcost improvement, skip 
+				if (gScore_temp >= searchValueSet[neighbor.first].gScore) continue;
+
+				// keep track of this step
+				cameFrom[neighbor.first] = current;
+				// set new values for neighbour's gScore and fScore
+				searchValueSet[neighbor.first] = SearchValues{ gScore_temp, gScore_temp + task.Heuristic(neighbor.first) };
+			}
+		}
+
+		return false;
+	}
+
+	template<class N, class E>
+	void ReconstructPath(std::unordered_map<IGraphNode<N, E>*, IGraphNode<N, E>*>& cameFrom, IGraphNode<N, E>* current, std::vector<IGraphNode<N, E>*>& path, float* cost)
+	{
+		*cost = 0;
+		path.clear();
+
+		IGraphNode<N, E>* next = current;
+
+		while (cameFrom.find(next) != cameFrom.end()) {
+			*cost += cameFrom[next]->GetEdge(next)->Cost;
+			path.push_back(next);
+			next = cameFrom[next];
+		}
+
+		std::reverse(path.begin(), path.end());
+	}
 }
