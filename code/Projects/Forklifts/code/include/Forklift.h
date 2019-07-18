68f0aefa547ad7f0a003f381f7475e270cc44bcf
Andreas Schmidt <a.schmidt@flexus.net>
2019-07-18 16:56:23 +0200

    Forklift: Implemented base class Forklift for graph movement

--- code/Projects/Forklifts/code/include/Forklift.h
+++ code/Projects/Forklifts/code/include/Forklift.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "AIngine/Core.h"
+#include "Tasks.h"
+#include "Graph.h"
 
 namespace Forklifts {
 	class Forklift : public AIngine::Script {
@@ -11,11 +13,44 @@ namespace Forklifts {
 		virtual void Update(float delta) override;
 		virtual void OnEventData(AIngine::Events::EventData& e) override;
 
-	private:
-		virtual void OnCollision(AIngine::Physics::Contact contact);
-		AIngine::Events::EventHandler<void, AIngine::Physics::Contact> OnCollisionEventHandler;
+		Task* currentTask = nullptr;
+		std::vector<IGraphNode<NodeData, EdgeData>*> path;
 
+		virtual void ReceiveTask(Task* task);
+
+		AIngine::Events::Event<void, Forklift*> OnTaskFinishedEvent;
+		typedef AIngine::Events::EventHandler<void, Forklift*> OnTaskFinishedEventHandler;
+		GraphNode* _currentNode = nullptr;
+		GraphNode* _nextNode = nullptr;
+		IEdge<EdgeData>* _currentEdge = nullptr;
+		Node* NodeInContact = nullptr;
 		int currentTaskIndex = 0;
+
+
+	protected:
+		virtual void MoveAlongPath();
+		virtual void MoveAlongPathRecursively(float* amount);
+		virtual void Move();
+		virtual void Move(float* value);
+		virtual void OnNodeReached() {}
+		virtual void FinishTask() {}
+		virtual void ProcessTask() {}
+
+	protected:
+		Graph* m_graph;
+		float maxSpeed = 4;
+		float minDistance = 0.06f;
+
+	private:
+		virtual void OnCollisionBegin(AIngine::Physics::Contact contact);
+		virtual void OnCollisionEnd(AIngine::Physics::Contact contact);
+
+		bool IsMissingTarget(const glm::vec2& direction, float* amount, float value = 0);
+		bool NearlyEqual(const glm::vec2& a, const glm::vec2& b, float tolerance = 0.01f);
+
+	private:
+		AIngine::Events::EventHandler<void, AIngine::Physics::Contact> OnCollisionBeginEventHandler;
+		AIngine::Events::EventHandler<void, AIngine::Physics::Contact> OnCollisionEndEventHandler;
 
 	};
 }
--- code/Projects/Forklifts/code/src/Forklift.cxx
+++ code/Projects/Forklifts/code/src/Forklift.cxx
@@ -1,5 +1,6 @@
 #include "Forklift.h"
 #include "Node.h"
+#include "Forklifts.h"
 
 namespace Forklifts {
 
@@ -13,20 +14,26 @@ namespace Forklifts {
 	// Start is called when gameplay starts for this script
 	void Forklift::OnStart()
 	{
-		OnCollisionEventHandler = AIngine::Events::EventHandler<void, AIngine::Physics::Contact>(std::bind(&Forklift::OnCollision, this, std::placeholders::_1));
-		GetOwner()->GetComponent<AIngine::Physics::PhysicsComponent>()->OnCollisionBegin += OnCollisionEventHandler;
+		OnCollisionBeginEventHandler = AIngine::Events::EventHandler<void, AIngine::Physics::Contact>(std::bind(&Forklift::OnCollisionBegin, this, std::placeholders::_1));
+		OnCollisionEndEventHandler = AIngine::Events::EventHandler<void, AIngine::Physics::Contact>(std::bind(&Forklift::OnCollisionEnd, this, std::placeholders::_1));
+		GetOwner()->GetComponent<AIngine::Physics::PhysicsComponent>()->OnCollisionBegin += OnCollisionBeginEventHandler;
+		GetOwner()->GetComponent<AIngine::Physics::PhysicsComponent>()->OnCollisionBegin += OnCollisionEndEventHandler;
 		GetOwner()->GetComponent<AIngine::Physics::PhysicsComponent>()->SetEnabled(true);
+
+		m_graph = AIngine::World::GetGameObject("Graph")->GetComponent<Graph>();
 	}
 
 	// End is called when gameplay ends for this script
 	void Forklift::OnEnd()
 	{
-		GetOwner()->GetComponent<AIngine::Physics::PhysicsComponent>()->OnCollisionBegin -= OnCollisionEventHandler;
+		GetOwner()->GetComponent<AIngine::Physics::PhysicsComponent>()->OnCollisionBegin -= OnCollisionBeginEventHandler;
+		GetOwner()->GetComponent<AIngine::Physics::PhysicsComponent>()->OnCollisionEnd -= OnCollisionEndEventHandler;
 	}
 
 	// Update is called once per frame
 	void Forklift::Update(float delta)
 	{
+
 	}
 
 	// Callback for events
@@ -35,11 +42,130 @@ namespace Forklifts {
 	}
 
 
-	void Forklift::OnCollision(AIngine::Physics::Contact contact)
+	void Forklift::ReceiveTask(Task * task)
+	{
+		currentTask = task;
+		path = task->Path;
+		ProcessTask();
+	}
+
+	void Forklift::MoveAlongPath()
+	{
+		if (!_nextNode) return;
+
+		if (NearlyEqual(m_graph->NodeMap[_nextNode]->GetOwner()->GetWorldPosition(), GetOwner()->GetWorldPosition(), minDistance)) {
+			// reached final node
+			if (_nextNode == currentTask->endNode->node) {
+				FinishTask();
+				return;
+			}
+			else {
+				OnNodeReached();
+			}
+		}
+
+		Move();
+	}
+
+	void Forklift::MoveAlongPathRecursively(float * amount)
+	{
+		if (!_nextNode || *amount <= std::numeric_limits<float>::epsilon()) return;
+
+		glm::vec2 nextnodepos = m_graph->NodeMap[_nextNode]->GetOwner()->GetWorldPosition();
+		glm::vec2 playerpos = GetOwner()->GetWorldPosition();
+
+		if (NearlyEqual(nextnodepos, playerpos, minDistance))
+		{
+			// reached final node
+			if (_nextNode == currentTask->endNode->node) {
+				FinishTask();
+				return;
+			}
+			else {
+				OnNodeReached();
+			}
+		}
+
+		Move(amount);
+	}
+
+	void Forklift::Move()
+	{
+		if (!_nextNode) return;
+
+		glm::vec2 direction = glm::normalize(m_graph->NodeMap[_nextNode]->GetOwner()->GetWorldPosition() - GetOwner()->GetWorldPosition());
+		glm::vec2 nextnodepos = m_graph->NodeMap[_nextNode]->GetOwner()->GetWorldPosition();
+		float amount = 0;
+
+		if (IsMissingTarget(direction, &amount)) {
+			GetOwner()->SetWorldPosition(nextnodepos);
+			MoveAlongPathRecursively(&amount);
+			return;
+		}
+		else {
+			GetOwner()->SetWorldPosition(GetOwner()->GetWorldPosition() + (direction * maxSpeed * (float)Forklifts::FIXED_TIMESTEP));
+		}
+	}
+
+	void Forklift::Move(float * value)
+	{
+		if (!_nextNode) return;
+
+		glm::vec2 nextnodepos = m_graph->NodeMap[_nextNode]->GetOwner()->GetWorldPosition();
+		glm::vec2 playerpos = GetOwner()->GetWorldPosition();
+		glm::vec2 direction = glm::normalize(nextnodepos - playerpos);
+
+		float amount = 0;
+
+		if (IsMissingTarget(direction, &amount)) {
+			GetOwner()->SetWorldPosition(nextnodepos);
+			MoveAlongPathRecursively(&amount);
+			return;
+		}
+		else {
+			GetOwner()->SetWorldPosition(playerpos + (direction * *value));
+		}
+	}
+
+	bool Forklift::NearlyEqual(const glm::vec2 & a, const glm::vec2 & b, float tolerance)
+	{
+		return std::abs(a.x - b.x) <= tolerance
+			&& std::abs(a.y - b.y) <= tolerance;
+	}
+
+	void Forklift::OnCollisionBegin(AIngine::Physics::Contact contact)
 	{
 		if (contact.Other && contact.Other->GetComponent<Node>())
 		{
-			m_nodeReached = true;
+			NodeInContact = contact.Other->GetComponent<Node>();
 		}
 	}
+
+	void Forklift::OnCollisionEnd(AIngine::Physics::Contact contact)
+	{
+		if (contact.Other && contact.Other->GetComponent<Node>())
+		{
+			NodeInContact = nullptr;
+		}
+	}
+
+	bool Forklift::IsMissingTarget(const glm::vec2 & direction, float * amount, float value)
+	{
+		glm::vec2 nextnodePos = m_graph->NodeMap[_nextNode]->GetOwner()->GetWorldPosition();
+		glm::vec2 currentPos = GetOwner()->GetWorldPosition();
+		glm::vec2 nextPosition = value > std::numeric_limits<float>::epsilon() ? currentPos + direction * value
+			: currentPos + (direction * maxSpeed *(float)Forklifts::FIXED_TIMESTEP);
+		glm::vec2 newDirection = glm::normalize(nextPosition - nextnodePos);
+
+		if (NearlyEqual(direction, newDirection, 0.01f)) {
+			*amount = glm::distance(nextPosition, nextnodePos);
+			return true;
+		}
+		*amount = 0;
+		return false;
+	}
+
+}
+namespace AIngine::Events {
+	int EventHandler<void, Forklifts::Forklift*>::counter = 0;
 }
